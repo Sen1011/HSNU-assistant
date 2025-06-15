9213dd83ff639bb614753c0526a907c12a92bbf4
sen <linushuang1011@gmail.com>
2025-06-15 14:45:11 +0800

    Add initial implementation of the assistant app with chat functionality

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from chat import *
+
+st.set_page_config(
+        page_title="師大附中小助手",
+        page_icon="school",
+        layout="wide",
+    )
+#圖片
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq0_NPKm8RiU5exq59qsYZoqLTfgpzmghjig&s",width=100)
+st.header("師大附中小助手", divider='rainbow')
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+if prompt := st.chat_input("今晚，我想來點期末考日期與考程"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    
+    response = f"你最棒的助手: {get_response(prompt)}"#把前面的prompt改成要輸出的東西
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+        # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
