8ea360a302477c78d63dcdbdb93afe7ed3c4c702
sen <linushuang1011@gmail.com>
2025-06-15 23:30:03 +0800

    Refactor API key retrieval and enhance response generation configuration in chat module

--- chat.py
+++ chat.py
@@ -1,10 +1,16 @@
 from google import genai
+from google.genai import types
 from semantic_retrieval import semantic_retrieval
 import os
 from time import asctime
 
 # Retrieve API key from environment variable
 google_api_key = os.environ.get("GEMINI_API_KEY")
+if not google_api_key:
+    from dotenv import load_dotenv
+    load_dotenv()
+    google_api_key = os.environ.get("GEMINI_API_KEY")
+
 client = genai.Client(api_key=google_api_key)
 
 def get_response(user_question):
@@ -27,7 +33,15 @@ def get_response(user_question):
     # Get Response
     response = client.models.generate_content(
         model="gemini-2.0-flash",
-        contents=prompt_template)
+        config=types.GenerateContentConfig(
+            system_instruction=f"""
+            It's {asctime()} now. You are a helpful assistant for 國立臺灣師範大學附屬高級中學(HSNU)'s student. You should answers questions based on the provided context.
+            Answer the question as detailed as possible from the provided context, and please use 繁體中文，臺灣用語,
+            make sure to provide all the details!
+            if the answer is not in provided context, just told the user that you don't know, don't provide the wrong answer
+            Provided Context:\n{str(docs)}"""
+        ),
+        contents=user_question)
     
     return response.text
 
